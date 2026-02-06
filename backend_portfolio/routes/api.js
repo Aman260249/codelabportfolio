e27d4e40a457a993d0abb92cf48ca0cd3759e82f
Aman260249 <aman26024@gmail.com>
2026-02-06 16:58:21 +0530

    Fixed start

--- backend_portfolio/routes/api.js
+++ backend_portfolio/routes/api.js
@@ -40,6 +40,17 @@ router.post('/contact', async (req, res) => {
 
 const Admin = require('../models/Admin');
 
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const newAdmin = new Admin({ username, password });
+        await newAdmin.save();
+        res.status(201).json({ success: true, message: "Admin Registered Successfully!" });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     try {
