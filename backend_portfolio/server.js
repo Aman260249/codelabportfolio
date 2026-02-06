2a3d1474a60c25ca54daec02d1ae8a2d0ef0e1da
Aman260249 <aman26024@gmail.com>
2026-02-06 16:53:34 +0530

    Fixed start script for server js

--- backend_portfolio/server.js
+++ backend_portfolio/server.js
@@ -15,6 +15,7 @@ app.use(cors({
 
 app.use(express.json()); // Body parser 
 app.use('/api', apiRoutes);
+app.get("/", (req, res) => res.send("Server is running! 🚀"));
 
 
 
@@ -22,8 +23,9 @@ app.use('/api', apiRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Atlas connected!"))
   .catch((err) => console.log("DB Connection Error:", err));
+  
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
